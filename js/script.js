820c423c15f860f13233cce2a8c093982128a0bd
Maksim Sobolev <makseamsobolev@gmail.com>
2021-02-02 00:35:27 +0300

    Сделал второе пз!

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
-alert('ўьў!');
-console.log(1);
+let money = 100000000;
+let  income = 'проституция';
+let  addExpenses = 'РеКлАмА, ПиАр, АрЕнДа';
+let  deposit = true;
+let  mission = 400000000;
+let  period = 3;
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit); //можно ли было все эти 3 строки вывести в одну? 
+//По аналогии как ты показывал в комментариях.
+console.log('В этих словах ' + addExpenses.length + ' символ');
+console.log('Период равен ' + period  + ' месяцам' + ' и цель - заработать ' + mission + ' мильярдов BYNов');
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split()); 
+let budgetDay = 100000000 / 30;
+console.log('вот столько будет в день зарабатываться ' + budgetDay);
+//сложное потом досдам, а то спать охота! Пока что знания нулевые, но все кажется довольно простым.
