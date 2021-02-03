95349b35e50be6211f34e6c1f6bffe4d6886203a
Maksim Sobolev <makseamsobolev@gmail.com>
2021-02-03 18:20:10 +0300

    сохранение перед удалением

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ console.log('В этих словах ' + addExpenses.length + ' символ');
 console.log('Период равен ' + period  + ' месяцам' + ' и цель - заработать ' + mission + ' мильярдов BYNов');
 console.log(addExpenses.toLowerCase.split());
 console.log(addExpenses.split()); 
-let budgetDay = 100000000 / 30;
+let budgetDay = money / 30;
 console.log('вот столько будет в день зарабатываться ' + budgetDay);
 /*сложное потом досдам, а то спать охота! Пока что знания нулевые, но все кажется довольно простым.
 P.S 
