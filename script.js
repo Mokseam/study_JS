a5363c43478c115c662e949596828ac7eab1ef25
Mokseam <48885754+Mokseam@users.noreply.github.com>
2021-02-03 21:37:36 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let money = 100000000;
+let  income = 'проституция';
+let  addExpenses = 'РеКлАмА, ПиАр, АрЕнДа';
+let  deposit = true;
+let  mission = 400000000;
+let  period = 3;
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit); //можно ли было все эти 3 строки вывести в одну? 
+//По аналогии как ты показывал в комментариях.
+console.log('В этих словах ' + addExpenses.length + ' символ');
+console.log('Период равен ' + period  + ' месяцам' + ' и цель - заработать ' + mission + ' мильярдов BYNов');
+console.log(addExpenses.toLowerCase.split());
+console.log(addExpenses.split()); 
+let budgetDay = money / 30;
+console.log('вот столько будет в день зарабатываться ' + budgetDay);
+/*сложное потом досдам, а то спать охота! Пока что знания нулевые, но все кажется довольно простым.
+P.S 
+$ git checkout -b lesson_2
+fatal: A branch named 'lesson_2' already exists.
+это как? Я не создавал такой бранч..
+*/
+
